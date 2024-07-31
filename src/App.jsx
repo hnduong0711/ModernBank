41202b94927004ecdcab96c1d4e7c4d34ff666a7
DuongTienSinh <huynhnhutduong7112003@gmail.com>
2024-07-31 21:28:24 +0700

    Build structure components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-// import React from 'react'
-
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import styles from "./style"
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <h1 className={`${styles.paddingX}`}>
+      Hello
+    </h1>
+  </div>
+)
 
 export default App
