7e907e5723e2b7cbe9a4bc2c22e53545b083dc83
DuongTienSinh <huynhnhutduong7112003@gmail.com>
2024-08-06 11:09:56 +0700

    Feat: Navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import styles from "./style"
+import { Navbar, Billing, CardDeal, Business, Clients, CTA, Stats, Footer, Testimonials, Hero } from './components'
+
 const App = () => (
-  <div className="bg-primary w-full overflow-hidden">
-    <h1 className={`${styles.paddingX}`}>
-      Hello
-    </h1>
+  <div className="bg-slate-400 w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
   </div>
 )
 
